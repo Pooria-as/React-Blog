6164ef11af387e0c9422b19d5dd240fa15140706
Pooria <p.asiabi@gmail.com>
2022-06-11 17:01:20 +0430

    Add final section contact page ✔

--- src/Pages/ContactUs.js
+++ src/Pages/ContactUs.js
@@ -1,18 +1,40 @@
 import React from 'react'
 import { motion } from 'framer-motion'
 import { PageAnimation } from '../components/Animate/Animation'
+import styled from 'styled-components'
 
 const ContactUs = () => {
   return (
-    <motion.div
-    variants={PageAnimation}
-    initial='hidden'
-    animate='show'
-    end='end'
-  >
+    <Contact variants={PageAnimation} initial='hidden' animate='show' end='end'>
       <h2>ContactUs</h2>
-    </motion.div>
+      <ul>
+        <li>
+          <h4>Tell : 09128633142</h4>
+        </li>
+        <li>
+          <h4>Email : p.asiabi@gmail.com</h4>
+        </li>
+        <li>
+          <h4>Tell : 02122005469</h4>
+        </li>
+      </ul>
+    </Contact>
   )
 }
+
+const Contact = styled(motion.div)`
+
+text-align: center;
+h2{
+  color: white;
+  font-size: 4rem;
+}
+ul{
+  list-style: none;
+  li{
+    color:white
+  }
+}
+`
 
 export default ContactUs
